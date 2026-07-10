60ba9ea1d58e16e6b9a0bc3bcd7e89e0c197be19
black <black@example.com>
2026-07-10 17:11:10 +0500

    Change greeting

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
+Console.WriteLine("hello everyone");
+
 void greetWhite()
 {
     Console.WriteLine("hello white");
